0b626ba377b230a0cf27beccc2a3da7129b1ac0c
Fahim2023 <fahimshahrier2023@gmail.com>
2025-11-13 15:43:30 +0600

    added deployment in my server

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("art-db");
     const artsCollection = db.collection("arts");
@@ -190,7 +190,7 @@ async function run() {
       res.send({ success: true, updatedArt });
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
