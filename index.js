c6ce062ba41df678ce89f16b3655d80560bd7420
Fahim2023 <fahimshahrier2023@gmail.com>
2026-01-03 15:54:11 +0600

    remover the mongodb url from comment

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 //cqBWotPooDyKcBTx
 
 // const uri =
-//   "mongodb+srv://simpleArtworks:cqBWotPooDyKcBTx@flash0.nw85ito.mongodb.net/?appName=Flash0";
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@flash0.nw85ito.mongodb.net/?appName=Flash0`;
 
 const client = new MongoClient(uri, {
